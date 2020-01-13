641caec7f2d62e9e668020df1131fb68893a056f
zhanglf <zhanglf@inhand.com.cn>
2020-01-13 14:05:52 +0800

    修改链接

--- lib/ios/AMap3D/maps/AMapMarkerManager.m
+++ lib/ios/AMap3D/maps/AMapMarkerManager.m
@@ -30,9 +30,9 @@ RCT_EXPORT_VIEW_PROPERTY(image, NSString)
 
 RCT_EXPORT_VIEW_PROPERTY(onPress, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onInfoWindowPress, RCTBubblingEventBlock)
-RCT_EXPORT_VIEW_PROPERTY(onDragStart, RCTBubblingEventBlock)
-RCT_EXPORT_VIEW_PROPERTY(onDrag, RCTBubblingEventBlock)
-RCT_EXPORT_VIEW_PROPERTY(onDragEnd, RCTBubblingEventBlock)
+// RCT_EXPORT_VIEW_PROPERTY(onDragStart, RCTBubblingEventBlock)
+// RCT_EXPORT_VIEW_PROPERTY(onDrag, RCTBubblingEventBlock)
+// RCT_EXPORT_VIEW_PROPERTY(onDragEnd, RCTBubblingEventBlock)
 
 RCT_EXPORT_METHOD(lockToScreen:(nonnull NSNumber *)reactTag x:(int)x y:(int)y) {
     [self.bridge.uiManager addUIBlock:^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
